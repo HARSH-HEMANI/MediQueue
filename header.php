216139e151a8ed93278d9022c485cf7996f2b05c
harsh hemani <hhemani365@gmail.com>
2026-01-11 20:41:58 +0530

    day1 #MediQueue wrote HTML and CSS for home page

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <nav>
+        <ul>
+            <span class="logo">
+                <img src="./img/mediq-logo.jpeg" alt="logo">
+            </span>
+            <span class="option">
+                <button class="btn btn-primary m-3" style="height: 35px; width:160px; margin: 5px;"><a href="login_book_appointment.php" style="color: #fff;">Book Appointment</a></button>
+
+                <button type="button" class="btn btn-outline-primary m-3" onclick="window.location.href='login.php'" style="height: 35px; width: 60px; margin: 5px;">Login</button>
+
+                <button class="btn btn-outline-primary m-3" onclick="window.location.href='registration.php'" style="height: 35px; width: 80px; margin: 5px">Register</button>
+            </span>
+        </ul>
+    </nav>
+
+</html>
